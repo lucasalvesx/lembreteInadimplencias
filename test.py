576aa67cc432a532048149dd606563e45a54c2ab
Lucas MIC <suporte.ti@micdigital.com.br>
2025-02-06 16:09:01 -0300

    TODO: preserve credentials on root file (.env) or gitignore

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import pandas as pd
+import yagmail 
 
 df = pd.read_excel(".\clientesInadimplencia.xlsx")
+ListaDestinatarios = df
 
 print(df.head())
+
+yag = yagmail.SMTP('comunicado@micdigital.com.br', '####')
+yag.send(
+    to = ListaDestinatarios
+    subject = 'Lembrete pagamento honorários'
+    contents = 'Lembramos que consta um pagamento em aberto' 
+    )
+
+# error handle
+
